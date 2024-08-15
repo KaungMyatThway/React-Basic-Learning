7a898322c26f6ead63a3603330cbcf8e582a1742
Kaung Myat Thway <kaungmyatthway1997@gmail.com>
2024-08-15 16:36:51 +0630

    react form learning

--- yaycha/src/App.jsx
+++ yaycha/src/App.jsx
@@ -1,28 +1,55 @@
 import { useState } from "react";
 import List from "./List.jsx";
 import Item from "./Item.jsx";
-
+import Form from "./Form";
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
   const [data, setData] = useState([
     { id: 1, content: "Hello World", name: "Kaung" },
     { id: 2, content: "Hello World", name: "Myat" },
     { id: 3, content: "Hello World", name: "Thway" },
   ]);
 
-  const remove = id => {
-    setData(data.filter(item => item.id !== id));
-  }
+  const remove = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const add = (content, name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, { id, content, name }]);
+  };
 
   return (
-    <div style={{ margin: "600px auto" }}>
-      <h1>Yaycha</h1>
+    <div style={{ margin: "0 auto" }}>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        Yaycha
+        <button
+          onClick={() => {
+            setShowForm(!showForm);
+          }}
+          style={{
+            width: 32,
+            height: 32,
+            borderRadius: 50,
+            background: showForm ? "green" : "red",
+            color: "white",
+          }}
+        >
+          {showForm ? "*" : "+"}
+        </button>
+      </h1>
+      {showForm && <Form add={add} />}
       <List>
-          {
-            data.map(item => {
-              return <Item key={item.id} item={item} remove={remove} />
-            })
-          }
+        {data.map((item) => {
+          return <Item key={item.id} item={item} remove={remove} />;
+        })}
       </List>
     </div>
   );
--- yaycha/src/Form.jsx
+++ yaycha/src/Form.jsx
@@ -4,9 +4,29 @@ function Form({ add }) {
   const contentRef = useRef();
   const nameRef = useRef();
   return (
-    <form>
-      <input ref type="text" placeholder="Content"></input>
-      <input type="text" placeholder="Name"></input>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const content = contentRef.current.value;
+        const name = nameRef.current.value;
+
+        add (content, name);
+
+        e.currentTarget.reset();
+      }}
+    >
+      <input
+        ref={contentRef}
+        type="text"
+        placeholder="Content"
+        style={{ padding: 5 }}
+      ></input>
+      <input
+        ref={nameRef}
+        type="text"
+        placeholder="Name"
+        style={{ padding: 5 }}
+      ></input>
       <button type="submit">Post</button>
     </form>
   );
